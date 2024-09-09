502ccd7e4ca8ad5bf1024198bd31db4106c29ba8
vansaldi <vicent.ansaldi@hotmail.com>
2024-09-08 23:28:09 -0400

    Finalizacion
    Se añade una animación, para finalizar con la primera entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, MatButtonModule, MatDividerModule, MatIconModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild  } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import type { Animation } from '@ionic/angular';
+import { AnimationController, IonCard } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +10,37 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HomePage {
 
+  @ViewChild(IonCard, { read: ElementRef }) card: ElementRef<HTMLIonCardElement>;
+
+  private animation: Animation;
+
   user: string;
 
-  constructor(private activeroute: ActivatedRoute, private router:Router) {
+  constructor(private animationCtrl: AnimationController, private activeroute: ActivatedRoute, private router:Router) {
     this.activeroute.queryParams.subscribe(params => {
       if(this.router.getCurrentNavigation()!.extras.state){
-        //console.log(this.router.getCurrentNavigation()!.extras.state!['user']);
         this.user = this.router.getCurrentNavigation()!.extras.state!['user'];
       }
     });
+  }
+  
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl
+      .create()
+      .addElement(this.card.nativeElement)
+      .duration(1500)
+      .iterations(Infinity)
+      .direction('alternate')
+      .fromTo('background', 'blue', 'var(--background)');
+  }
+
+  play() {
+    if(this.animation.isRunning())
+    {   
+      this.animation.stop();
+    } else {
+      this.animation.play();
+    }
   }
 
 }
