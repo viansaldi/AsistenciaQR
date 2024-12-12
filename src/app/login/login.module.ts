9ec93d9c0eda7b86df51fd42e98deb395b2a5665
vansaldi <vicent.ansaldi@hotmail.com>
2024-12-12 10:53:01 -0400

    ANGULAR MAT

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -7,13 +7,19 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    MatButtonModule, 
+    MatDividerModule,
+    MatIconModule
   ],
   declarations: [LoginPage]
 })
